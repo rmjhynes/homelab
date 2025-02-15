6c9a6dd3189bd531bf1178bf56ed62956b6705bc
Ryan Hynes <131348960+rmjhynes@users.noreply.github.com>
2025-02-15 14:33:31 +0000

    Removed prometheus helm chart from install script as argo installs kube-prom stack anyway.

--- install/bootstrap.sh
+++ install/bootstrap.sh
@@ -1,10 +1,8 @@
-# Create namespaces
+# Create namespace
 kubectl create ns argocd
-kubectl create ns monitoring
 
-# Install Helm charts
+# Install Helm chart
 helm install argocd argo/argo-cd --namespace argocd
-helm install prometheus prometheus-community/kube-prometheus-stack --namespace monitoring
 
 # Apply Argo Helm AppProject resource
 kubectl apply -f project.yaml
