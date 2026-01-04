eced687f5111f3a2f6779981a6becafec4cc6cc1
Ryan Hynes <131348960+rmjhynes@users.noreply.github.com>
2026-01-04 21:31:27 +0000

    feat: remove unnecessary variables

--- install/terraform/main.tf
+++ install/terraform/main.tf
@@ -1,29 +1,25 @@
-# Create the ArgoCD namespace
 resource "kubernetes_namespace" "argocd" {
   metadata {
-    name = var.argocd_namespace
+    name = "argocd"
   }
 }
 
-# Install ArgoCD via Helm
 resource "helm_release" "argocd" {
   name       = "argocd"
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
   namespace  = kubernetes_namespace.argocd.metadata[0].name
-  version    = var.argocd_chart_version != "" ? var.argocd_chart_version : null
+  version    = "latest"
 
   depends_on = [kubernetes_namespace.argocd]
 }
 
-# Apply the AppProject manifest
 resource "kubernetes_manifest" "argocd_project" {
   manifest = yamldecode(file("${path.module}/../project.yaml"))
 
   depends_on = [helm_release.argocd]
 }
 
-# Apply the root Application manifest
 resource "kubernetes_manifest" "argocd_applications" {
   manifest = yamldecode(file("${path.module}/../applications.yaml"))
 
--- install/terraform/variables.tf
+++ install/terraform/variables.tf
@@ -4,14 +4,3 @@ variable "kubeconfig_path" {
   default     = "/etc/rancher/k3s/k3s.yaml"
 }
 
-variable "argocd_chart_version" {
-  description = "Version of the ArgoCD Helm chart (leave empty for latest)"
-  type        = string
-  default     = ""
-}
-
-variable "argocd_namespace" {
-  description = "Namespace for ArgoCD"
-  type        = string
-  default     = "argocd"
-}
