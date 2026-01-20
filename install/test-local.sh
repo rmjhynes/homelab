b39076169b638254b819fabac59f346d020abebf
Ryan Hynes <131348960+rmjhynes@users.noreply.github.com>
2026-01-20 21:51:36 +0000

    style: change info log from green to cyan

--- install/test-local.sh
+++ install/test-local.sh
@@ -11,12 +11,12 @@ TARGET_REVISION=$(git branch --show-current)
 
 # Colours for output
 RED='\033[0;31m'
-GREEN='\033[0;32m'
+CYAN='\033[0;36m'
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Colour
 
 # Render log text with specific colour depending on log type
-log_info() { echo -e "${GREEN}[INFO]${NC} $1"; }
+log_info() { echo -e "${CYAN}[INFO]${NC} $1"; }
 log_warn() { echo -e "${YELLOW}[WARN]${NC} $1"; }
 log_error() { echo -e "${RED}[ERROR]${NC} $1"; }
 
