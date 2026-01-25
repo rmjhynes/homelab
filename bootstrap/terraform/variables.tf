5ad1bf2bd94d179439a58f5fc7c6b9c48939cb7f
Ryan Hynes <131348960+rmjhynes@users.noreply.github.com>
2026-01-25 12:52:25 +0000

    feat: update path to kubeconfig in terraform

--- bootstrap/terraform/variables.tf
+++ bootstrap/terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "kubeconfig_path" {
   description = "Path to the kubeconfig file"
   type        = string
-  default     = "/etc/rancher/k3s/k3s.yaml"
+  default     = "/home/rmjhynes/.kube/config"
 }
 
 variable "target_revision" {
