124e9e2429e3020535ed73042e312a484fe0ac49
Ryan Hynes <131348960+rmjhynes@users.noreply.github.com>
2026-07-08 21:31:17 +0100

    style: add comments and make Terraform expression clearer

--- bootstrap/terraform/main.tf
+++ bootstrap/terraform/main.tf
@@ -11,7 +11,7 @@ resource "helm_release" "argocd" {
   namespace  = kubernetes_namespace.argocd.metadata[0].name
   # Must match the version in applications/argocd/application.yaml, otherwise
   # the self-managed ArgoCD immediately replaces the bootstrapped install
-  version    = "7.7.16"
+  version = "7.7.16"
 
   depends_on = [kubernetes_namespace.argocd]
 }
@@ -23,11 +23,12 @@ resource "kubernetes_manifest" "argocd_project" {
 }
 
 resource "kubernetes_manifest" "argocd_applications" {
-  # selfHeal is disabled when testing a branch; see the comment on selfHeal in
-  # the template
   manifest = yamldecode(templatefile("${path.module}/../applications.yaml.tftpl", {
     target_revision = var.target_revision
-    self_heal       = var.target_revision == "HEAD"
+    # selfHeal is set to false when testing a branch so the child applications
+    # can be patched to point at the feature branch configurations, and the
+    # patches won't be reverted by ArgoCD
+    self_heal = var.target_revision == "HEAD" ? true : false
   }))
 
   depends_on = [helm_release.argocd]
